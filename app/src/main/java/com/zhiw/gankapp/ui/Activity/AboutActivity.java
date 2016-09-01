851389023a87c49df656b216b36d55c686813abe
Zhiw <karlkb81@gmail.com>
2016-09-01 23:01:40 +0800

    update about content

--- app/src/main/java/com/zhiw/gankapp/ui/Activity/AboutActivity.java
+++ app/src/main/java/com/zhiw/gankapp/ui/Activity/AboutActivity.java
@@ -3,8 +3,23 @@ package com.zhiw.gankapp.ui.activity;
 import com.zhiw.gankapp.R;
 import com.zhiw.gankapp.app.ToolBarActivity;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
+import butterknife.Bind;
+
 public class AboutActivity extends ToolBarActivity {
 
+
+    @Bind(R.id.toolbar_layout)
+    CollapsingToolbarLayout mToolbarLayout;
+    @Bind(R.id.tv_about_content)
+    TextView mTvAboutContent;
+    @Bind(R.id.fab)
+    FloatingActionButton mFab;
 
     @Override
     protected int getLayoutResId() {
@@ -13,6 +28,9 @@ public class AboutActivity extends ToolBarActivity {
 
     @Override
     protected void initPresenter() {
+        mTvAboutContent.setAutoLinkMask(Linkify.ALL);
+        mTvAboutContent.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
+
 }
